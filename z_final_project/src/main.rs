f144207cc1074fdca721d44644a249e808dd3da7
nguyenbuitk <buinguyen23112k@gmail.com>
2023-11-04 02:34:45 +0000

    update fram of final project

--- z_final_project/src/main.rs
+++ z_final_project/src/main.rs
@@ -1,5 +1,17 @@
+#![allow(dead_code, unused_imports, unused_variables, unused_must_use)]
 use std::error::Error;
+use std::thread;
 use rusty_audio::Audio;
+use crossterm::{event, execute, terminal, ExecutableCommand};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use std::time::Duration;
+use std::io;
+use std::sync::mpsc;
+use z_final_project::frame;
+use z_final_project::render;
+use z_final_project::frame::new_frame;
 
 fn main() -> Result <(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -9,8 +21,59 @@ fn main() -> Result <(), Box<dyn Error>> {
     audio.add("pew", "pew.wav");
     audio.add("startup", "startup.wav");
     audio.add("win", "win.wav");
-    audio.play("startup");
+    audio.play("pew");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide);
+
+    // Render loop in seperate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    // Game loop
+    'gameloop: loop {
+        // Per frame init
+        let curr_frame = new_frame();
+
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
--- z_final_project/src/render.rs
+++ z_final_project/src/render.rs
@@ -1,21 +1,22 @@
 // crossterm lib để hiển thị các khung hình (frames) trên một terminal trong Rust.
 use std::io::Stdout;
-use crate::fram::Frame;
+use std::io::Write;
+use crate::frame::Frame;
 use crossterm::QueueableCommand;
-use crossterm::style::{SetBackgroudColor, Color};
+use crossterm::style::{SetBackgroundColor, Color};
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::cursor::MoveTo;
 
 pub fn render(stdout: &mut Stdout, last_frame: &Frame, curr_frame: &Frame, force: bool) {
     if force {
-        stdout.queue(SetBackgroudColor(Color::Blue)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::Blue)).unwrap();
         stdout.queue(Clear(ClearType::All)).unwrap();
-        stdout.queue(SetBackgroundColoro(Color::Black)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
     }
 
     for (x, col) in curr_frame.iter().enumerate() {
         for (y, s) in col.iter().enumerate() {
-            if *s != last_fram[x][y] || force {
+            if *s != last_frame[x][y] || force {
                 stdout.queue(MoveTo(x as u16, y as u16)).unwrap();
                 print!("{}", *s);
             }
